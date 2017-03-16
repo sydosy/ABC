3725307ceb78a26ff76333110bcb2556cf0035ec
yamase0394 <raiden1192@gmail.com>
2017-03-16 23:08:55 +0900

    ホワイトリスト登録機能とホワイトリストに登録されているか検出する機能を作った

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -11,7 +11,33 @@ function run() {
             let inputElements = formElements[i].getElementsByTagName('input');
             for (let j = 0; j < inputElements.length; j++) {
                 if (inputElements[j].type === 'password') {
-                    alert('友利奈緒が可愛い');
+                    chrome.storage.local.get('testWhiteList', function (value) {
+                        let whiteList = value.testWhiteList;
+                        console.log(whiteList);
+                        if (whiteList != null) {
+                            for (let i = 0; i < whiteList.length; i++) {
+                                if (whiteList.indexOf(window.location.href) >= 0) {
+                                    return;
+                                }
+                            }
+                        }
+                        swal({
+                            text: 'データ通信方式がhttpです。外部からパスワード等が閲覧できる可能性があります。',
+                            input: 'checkbox',
+                            inputPlaceholder: '友利奈緒が可愛い',
+                            confirmButtonText: '確認'
+                        }).then(function (result) {
+                            if (result === 1) {
+                                if (!whiteList) {
+                                    whiteList = [];
+                                }
+                                whiteList.push(window.location.href);
+                                chrome.storage.local.set({'testWhiteList': whiteList}, function () {
+                                    console.log(whiteList);
+                                });
+                            }
+                        });
+                    });
                 }
             }
         }
