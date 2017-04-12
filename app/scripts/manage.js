bf6f8e25ae8d846d05ce445ca2e69eeaf5782b02
sydosy <sydosy@gmail.com>
2017-04-12 19:33:22 +0900

    whiteList削除のchrome.storage保存ができるようになった

--- app/scripts/manage.js
+++ app/scripts/manage.js
@@ -5,17 +5,25 @@ chrome.storage.local.get('whiteList', function (storage) {
         id: 'select-white-list',
         size: 10
     });
-    for(let url in whiteList) {
+    for (let url in whiteList) {
         selectList.append($('<option></option>', {
             value: url,
             text: url
         }));
     }
     list.append(selectList);
-});
 
-$(document).ready(function () {
     $('#del-button1').on('click', function () {
-        $('#select-white-list').find('option:selected').remove();
+        let removeUrl = $('#select-white-list').find('option:selected');
+        delete whiteList[removeUrl.val()];
+        chrome.storage.local.set({'whiteList': whiteList}, function () {
+            console.log('save!');
+        });
+        removeUrl.remove();
+        $('#del-button1').prop('disabled', true);
+    });
+
+    $('#select-white-list').change(function () {
+        $('#del-button1').prop('disabled', false);
     });
 });
